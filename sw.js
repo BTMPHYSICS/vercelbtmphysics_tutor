bb2eaa22ccd8175595692907266913c9d6693aa1
BTMPHYSICS <sometimeswhereiam@gmail.com>
2026-08-20 16:01:50 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
 
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => new Response('오프라인 상태입니다.')));
+});
